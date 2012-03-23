0d31bc5d5c19e88379a95eba5ba13b0240b3bfb9
Evandro Eisinger <evandroeisinger@gmail.com>
2012-03-23 17:50:05 -0300

    Html

---
+++ html/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>This is SPARTA</title>
+	<link rel="stylesheet/less" type="text/css" href="style/base.less">
+	<script type="text/javascript" src="script/less-1.3.0.min.js"></script>
+</head>
+<body>
+	<div class="center">
+		<div id="header">
+			<h1><img src="images/logo-agora.png" alt="Ágora" /></h1>
+			<a id="login" href="">Conectar-se</a>
+			<div id="user-info">
+				<img src="" />
+				<p>Nome do usuário</p>
+				<a href="">Sair</a>
+			</div>
+		</div>
+		<ul id="candidates">
+			<h2>Candidatos</h2>
+			<li>
+				<div class="thumb">
+					<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/368710_1664574483_389674048_n.jpg" />
+				</div>
+				<h3>Bruno Stein</h3>
+				<a class="support" href="">Apoiar</a>
+				<div class="counter">88</div>
+			</li>
+			<li>
+				<div class="thumb">
+					<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/275259_100000507365811_494642663_n.jpg" />
+				</div>
+				<h3>Evandro Eisinger</h3>
+				<a class="support" href="">Apoiar</a>
+				<div class="counter">88</div>
+			</li>
+			<li>
+				<div class="thumb">
+					<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/273869_100000435792178_1121551184_n.jpg" />
+				</div>
+				<h3>Filipe Guth</h3>
+				<a class="support" href="">Apoiar</a>
+				<div class="counter">88</div>
+			</li>
+			<li>
+				<div class="thumb">
+					<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/41771_725758823_6562_n.jpg" />
+				</div>
+				<h3>Gibran Malheiros</h3>
+				<a class="support" href="">Apoiar</a>
+				<div class="counter">88</div>
+			</li>
+		</ul>
+	</div>
+</body>
+</html>
