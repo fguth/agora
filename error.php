7a1bb455c3b8bfbd93579b3f56b6c3874a14301c
Gibran Malheiros <gibatronic@gmail.com>
2012-04-18 23:06:53 -0300

    fixed a mistake, system.php cannot be used within error.php

--- error.php
+++ error.php
@@ -1,25 +1,19 @@
 <?php
-require("php/system.php");
 require("php/direct-access.php");
 ?>
 
 <!DOCTYPE HTML>
 
 <html>
-
-<head>
-
-	<title>Ágora</title>
-	<base href="http://<?php print_r($_SERVER["HTTP_HOST"]); ?>" />
-	<link rel="stylesheet" type="text/css" href="css/prototype.css" />
-	<link rel="icon" type="image/png" href="images/favicon.ico" />
-	
-</head>
-
-<body>
-
-	<img src="images/4XX.gif" width=100%; height=100%; />
-	
-</body>
-
+	<head>
+		<title>Ágora</title>
+		
+		<base href="http://<?php print_r($_SERVER["HTTP_HOST"]); ?>" />
+		
+		<link rel="stylesheet" type="text/css" href="css/prototype.css" />
+		<link rel="icon" type="image/png" href="images/favicon.ico" />
+	</head>
+	<body>
+		<img src="images/4XX.gif" width="100%" height="100%" />
+	</body>
 </html>
