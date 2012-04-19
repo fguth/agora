2a9b8b1bfba738559f00c75b1ff9aab6ed5a791a
Gibran Malheiros <gibatronic@gmail.com>
2012-04-18 23:25:30 -0300

    better expression in the error message

--- error.php
+++ error.php
@@ -12,8 +12,29 @@ require("php/direct-access.php");
 		
 		<link rel="stylesheet" type="text/css" href="css/prototype.css" />
 		<link rel="icon" type="image/png" href="images/favicon.ico" />
+		
+		<style type="text/css">
+			body {
+				overflow: hidden;
+				margin: 0px;
+			}
+			
+			body, html {
+				height: 100%;
+			}
+			
+			div#content {
+				background-image: url("images/4XX.gif");
+				background-position: center middle;
+				background-repeat: repeat;
+				text-align: center;
+				height: 100%;
+			}
+		</style>
 	</head>
 	<body>
-		<img src="images/4XX.gif" width="100%" height="100%" />
+		<div id="content">
+			<img src="images/4XX.gif" height="100%" />
+		</div>
 	</body>
 </html>
