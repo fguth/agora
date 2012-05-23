66c58f6f4824d19b2f975756f7f669f3a1acb0c3
Bruno Henrique Stein <brunohstein@gmail.com>
2012-05-23 01:19:16 -0300

    hohoho, that's stupid

--- index.php
+++ index.php
@@ -501,10 +501,10 @@
 	
 	<script type="text/javascript" src="javascripts/library/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="javascripts/library/tooltipsy.min.js"></script>
-	<script type="text/javascript" src="javascripts/library/prefixfree.min.js"></script>
 	<script type="text/javascript" src="javascripts/library/sef.js"></script>
 	<script src="http://j.maxmind.com/app/geoip.js"></script> 
 	<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script type="text/javascript" src="javascripts/library/prefixfree.min.js"></script>
 	<script type="text/javascript" src="javascripts/config.js"></script>
 	<script type="text/javascript" src="javascripts/main.js"></script>
 	
