f602d3e23253259b7189f9fc79c317f1dd4b910b
Bruno Henrique Stein <brunohstein@gmail.com>
2012-05-30 01:04:42 -0300

    set my city animation and footer shining

--- hypertext-preprocessor/footer.php
+++ hypertext-preprocessor/footer.php
@@ -1,3 +1,17 @@
+			<div class="footer">
+				<img src="images/logo-agora-footer.png" alt="Ágora" class="footer__logo" />
+				<div class="footer__column">
+					<ul class="footer__links">
+						<li class="footer__links__item"><a href="" class="footer__links__item__link">Sobre</a></li>•
+						<li class="footer__links__item"><a href="" class="footer__links__item__link">Política de Privacidade</a></li>•
+						<li class="footer__links__item"><a href="" class="footer__links__item__link">Termos de uso</a></li>•
+						<li class="footer__links__item"><a href="" class="footer__links__item__link">Contato</a></li>
+					</ul>
+					<p class="footer__copyright">© Ágora 2012. Alguns direitos reservados.</p>
+				</div>
+				<img src="images/logo-agora-flag.png" alt="Ágora" class="footer__flag" />
+			</div>
+			
 		</div>
 
 	</body>
