1421ad191d2f69c3991da4afbd5282315e2c321d
Bruno Henrique Stein <brunohstein@gmail.com>
2012-09-13 22:32:44 -0300

    included warning on header

--- hypertext-preprocessor/header.php
+++ hypertext-preprocessor/header.php
@@ -28,6 +28,9 @@ $header	= new Header($auth->user);
 	<body data-token="<?php echo ($auth->token); ?>" data-id="<?php echo ($auth->id); ?>" data-content-type="<?php echo($header->content_type); ?>" data-path="<?php echo($header->path); ?>" data-city-id="<?php echo($header->city_id); ?>">
 		<div class="wrapper">
 			<div class="header">
+				<p class="header__warning">
+					<b>Importante:</b> O Ágora não se trata de pesquisa eleitoral, mas sim de um site para levantamento de opiniões, sem controle de amostra, o qual não utiliza método científico para a sua realização, dependendo, apenas, da participação espontânea do interessado. Art. 33 da Lei no 9.504/97
+				</p>
 				<div class="header__logo">
 					<h1><a href="http://<?php echo (HOST); ?>/<?php echo ($header->state_sa); ?>/<?php echo ($header->city_url); ?>">
 						<img src="images/logo-agora.png" alt="Ágora" title="Ágora" />
